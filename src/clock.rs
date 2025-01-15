f392b36c70e946d188fb62588981bfc2eb74c643
Kamil Malinowski <malinowskikam96@gmail.com>
2025-01-15 23:33:14 +0100

    Cleaned clock.rs

--- src/clock.rs
+++ src/clock.rs
@@ -1,11 +1,9 @@
 use crate::args::Args;
-use jiff::tz::TimeZone;
-use jiff::{Span, Timestamp, Unit, Zoned};
-use std::fs::File;
-use std::io::{stdout, Write};
-use std::thread::{sleep, spawn, JoinHandle};
-use std::time::Duration;
+use jiff::{Span};
+use std::thread::{spawn, JoinHandle};
 use crate::error::ClockError;
+use crate::output::Output;
+use crate::scheduler::{Scheduler, SystemTimeScheduler};
 
 pub fn start_clock(args: Args) -> JoinHandle<Result<(), ClockError>> {
     spawn(move || {
@@ -15,67 +13,20 @@ pub fn start_clock(args: Args) -> JoinHandle<Result<(), ClockError>> {
             "%H:%M:%S".to_string()
         };
 
-        let mut output = if let Some(path) = args.output {
-            Box::new(File::create(path).unwrap()) as Box<dyn Write>
-        } else {
-            Box::new(stdout()) as Box<dyn Write>
-        };
+        let mut output = Output::try_from_args(&args.output)?;
 
-        let mut events_n = args.number;
+        let span = Span::new().minutes(1);
+        let mut scheduler = SystemTimeScheduler::new(args.number, span)?;
 
-        let tz = TimeZone::system();
-        let mut timestamp_zoned = Zoned::now();
-
-        let next_minute_seconds = timestamp_zoned.timestamp().as_second()
-            + (60i64 - (timestamp_zoned.timestamp().as_second() % 60));
-        let mut next_minute_zoned =
-            Zoned::new(Timestamp::new(next_minute_seconds, 0).unwrap(), tz.clone());
-
-        while should_continue(&mut events_n) {
-            timestamp_zoned = Zoned::now();
-            if timestamp_zoned < next_minute_zoned {
-                let wait_span = (next_minute_zoned.timestamp() - timestamp_zoned.timestamp())
-                    .total(Unit::Millisecond)
-                    .unwrap();
-                assert!(wait_span > 0.0 && wait_span < u64::MAX as f64);
-                sleep(Duration::from_millis(wait_span as u64));
-            }
-
-            writeln!(
-                output,
-                "{}",
-                next_minute_zoned.strftime(&format).to_string()
-            )
-            .unwrap();
-
-            next_minute_zoned = Zoned::new(
-                next_minute_zoned.timestamp() + Span::new().minutes(1),
-                tz.clone(),
-            );
-
-        }
+        start_clock_loop(&format, &mut output, &mut scheduler)?;
         Ok(())
     })
 }
 
-/// The program should continue if the events_n option is unspecified or if it's specified
-/// and positive.
-///
-/// # Arguments
-///
-/// * `events_n`: Current events_n Option. Will be decremented if specified.
-///
-/// returns: ``true`` if the program should continue
-fn should_continue(events_n: &mut Option<usize>) -> bool {
-    match events_n {
-        Some(n) => {
-            if *n > 0 {
-                *n -= 1;
-                true
-            } else {
-                false
-            }
-        }
-        None => true,
+fn start_clock_loop(format: &str, output: &mut Output, scheduler: &mut dyn Scheduler) -> Result<(), ClockError> {
+    while scheduler.should_continue() {
+        let next_event = scheduler.wait_until_next_event()?;
+        output.write(&next_event.strftime(format).to_string())?;
     }
+    Ok(())
 }
